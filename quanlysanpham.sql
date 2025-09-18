94569a3ad1acecba6a87f8d61f6ebe40cb08d66b
thanh <hnt2k41234567890@gmail.com>
2025-09-18 11:13:18 +0700

    h

---
+++ quanlysanpham.sql
@@ -0,0 +1,234 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Máy chủ: 127.0.0.1
+-- Thời gian đã tạo: Th9 18, 2025 lúc 06:11 AM
+-- Phiên bản máy phục vụ: 10.4.32-MariaDB
+-- Phiên bản PHP: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Cơ sở dữ liệu: `quanlysanpham`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `failed_jobs`
+--
+
+CREATE TABLE `failed_jobs` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `uuid` varchar(255) NOT NULL,
+  `connection` text NOT NULL,
+  `queue` text NOT NULL,
+  `payload` longtext NOT NULL,
+  `exception` longtext NOT NULL,
+  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `migrations`
+--
+
+CREATE TABLE `migrations` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `migration` varchar(255) NOT NULL,
+  `batch` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `migrations`
+--
+
+INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
+(1, '2014_10_12_000000_create_products_table', 1),
+(2, '2014_10_12_000000_create_users_table', 1),
+(3, '2014_10_12_100000_create_password_resets_table', 1),
+(4, '2019_08_19_000000_create_failed_jobs_table', 1),
+(5, '2019_12_14_000001_create_personal_access_tokens_table', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `password_resets`
+--
+
+CREATE TABLE `password_resets` (
+  `email` varchar(255) NOT NULL,
+  `token` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `personal_access_tokens`
+--
+
+CREATE TABLE `personal_access_tokens` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `tokenable_type` varchar(255) NOT NULL,
+  `tokenable_id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `token` varchar(64) NOT NULL,
+  `abilities` text DEFAULT NULL,
+  `last_used_at` timestamp NULL DEFAULT NULL,
+  `expires_at` timestamp NULL DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `products`
+--
+
+CREATE TABLE `products` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `image` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`image`)),
+  `is_valid` tinyint(1) NOT NULL DEFAULT 1,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `description`, `price`, `image`, `is_valid`, `created_at`, `updated_at`) VALUES
+(4, 'Sản phẩm C', 'SPC', 10000.00, '\"[\\\"products\\\\\\/4ij4olcoefvUYs2tmpZgP1VNooWwjHfkfeUiexwx.png\\\",\\\"products\\\\\\/mOjcAQwln7Y92C9cIkdCNmlrsnNHohsHxTOx8MuN.png\\\"]\"', 1, '2025-09-15 21:20:17', '2025-09-17 10:49:12'),
+(5, 'Sản phẩm D', 'ff', 3333.00, '\"[\\\"products\\\\\\/s50r4R2I6se88KZtXm9yiUNpXuNKWCsC15aEt4Q4.png\\\"]\"', 1, '2025-09-15 21:32:39', '2025-09-15 21:32:39'),
+(10, 'Sản phẩm 1', 'Mô tả sản phẩm 1', 100000.00, '\"[\\\"products\\\\\\/5tCOADeXoEM44bgA1a4lHo60e1uqIHVPcQpNEMo3.png\\\"]\"', 1, '2025-09-17 16:29:54', '2025-09-17 20:25:59'),
+(11, 'Sản phẩm 2', 'Mô tả sản phẩm 2', 120000.00, '\"[\\\"products\\\\\\/1YClOM7Y2epdakVFDxTHrgiJKlKfzyOFiYMvEilg.png\\\"]\"', 1, '2025-09-17 16:29:54', '2025-09-17 20:14:34'),
+(12, 'Sản phẩm 3', 'Mô tả sản phẩm 3', 90000.00, '\"[\\\"products\\\\\\/JfDKf6Ndol55POnztM81HHt7zkwHkMdLXrjC9gbl.jpg\\\"]\"', 1, '2025-09-17 16:29:54', '2025-09-17 09:34:47'),
+(13, 'Sản phẩm 4', 'Mô tả sản phẩm 4', 150000.00, '\"[\\\"products\\\\\\/fxonTi23FgNGIbIn7hQOtvJMSh0irs2ZlihktWLL.jpg\\\"]\"', 0, '2025-09-17 16:29:54', '2025-09-17 09:34:54'),
+(14, 'Sản phẩm 5', 'Mô tả sản phẩm 5', 200000.00, '\"[\\\"products\\\\\\/sAlEnjychGqAsRXo2NHnos7UPrFk1RGGvRD4oLFP.jpg\\\"]\"', 1, '2025-09-17 16:29:54', '2025-09-17 09:35:01'),
+(15, 'Sản phẩm 6', 'Mô tả sản phẩm 6', 110000.00, '\"[\\\"products\\\\\\/XqxKdlwBBQKOFUIbXBrkWYbEB6iR10Ei7FXHUUr2.jpg\\\"]\"', 1, '2025-09-17 16:29:54', '2025-09-17 09:35:06'),
+(16, 'Sản phẩm 7', 'Mô tả sản phẩm 7', 130000.00, '\"[\\\"products\\\\\\/9NefKhFw7ZWxo3WOCubivq1MI8tSPOIH89Y6kDjV.jpg\\\"]\"', 0, '2025-09-17 16:29:54', '2025-09-17 09:35:23'),
+(20, 'Sản phẩm A', 'hdhdsch', 100000.00, '\"[\\\"products\\\\\\/108EXujl0tU2rC5LrTqL9ACQx1CkSxnvMujmY0BJ.png\\\"]\"', 1, '2025-09-17 10:51:09', '2025-09-17 10:51:09'),
+(21, 'Sản phẩm A', NULL, 10000.00, '\"[\\\"products\\\\\\/JYl7wJbpzHaRYl6aBi1hpiCSOaeiiS1qXJQlZ3aJ.png\\\"]\"', 1, '2025-09-17 20:41:26', '2025-09-17 20:41:26');
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `users`
+--
+
+CREATE TABLE `users` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `phone` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `users`
+--
+
+INSERT INTO `users` (`id`, `email`, `name`, `password`, `phone`, `created_at`, `updated_at`) VALUES
+(3, 'z890k1@gmail.com', 'test1', '$2y$10$JDeA271/z5nTSIfZdY3tzeSABA9oOu.fdnQNwf0VtUXZ1zDtkzG4e', '0987654321', '2025-09-17 04:11:39', '2025-09-17 04:11:39'),
+(8, 'hnt2k41234567890@gmail.com', 'thanh', '$2y$10$hHsmbxjrckskb0uruJ5hguZFeiu9ACQtSzv3I8aUOvEtL.Loe1EIW', '0987654321', '2025-09-17 04:47:54', '2025-09-17 04:47:54'),
+(9, 'zmrt1234567890k1@gmail.com', 'test', '$2y$10$P.YjDtkD4zXzUTQggFKCQe3v6eeG/N/3PIArSTChjtaiBDFC.vyki', '0987654321', '2025-09-17 20:27:32', '2025-09-17 20:27:32');
+
+--
+-- Chỉ mục cho các bảng đã đổ
+--
+
+--
+-- Chỉ mục cho bảng `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+
+--
+-- Chỉ mục cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `password_resets`
+--
+ALTER TABLE `password_resets`
+  ADD PRIMARY KEY (`email`);
+
+--
+-- Chỉ mục cho bảng `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `personal_access_tokens_token_unique` (`token`),
+  ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
+
+--
+-- Chỉ mục cho bảng `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Chỉ mục cho bảng `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `users_email_unique` (`email`);
+
+--
+-- AUTO_INCREMENT cho các bảng đã đổ
+--
+
+--
+-- AUTO_INCREMENT cho bảng `failed_jobs`
+--
+ALTER TABLE `failed_jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `migrations`
+--
+ALTER TABLE `migrations`
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT cho bảng `personal_access_tokens`
+--
+ALTER TABLE `personal_access_tokens`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+
+--
+-- AUTO_INCREMENT cho bảng `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
